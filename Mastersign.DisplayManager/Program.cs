7eff04051b00879af1c0d44a4d746774e2902958
Tobias Kiertscher <dev@mastersign.de>
2017-11-11 10:37:16 +0100

    Projektdateien hinzufügen.

---
+++ Mastersign.DisplayManager/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastersign.DisplayManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
