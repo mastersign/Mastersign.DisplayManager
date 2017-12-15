7a381a2945688a09a43da3b2fdc93d4816581ad2
Tobias Kiertscher <dev@mastersign.de>
2017-12-15 13:36:12 +0100

    implemented new argument interpretation
    - added help text
    - added version info
    - added support for persistent loading
    - added support for reset

--- Mastersign.DisplayManager/Program.cs
+++ Mastersign.DisplayManager/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Xml.Serialization;
 
@@ -13,53 +14,114 @@ namespace Mastersign.DisplayManager
     {
         static int Main(string[] args)
         {
-            if (args.Length < 1)
+            var argParser = new ArgumentParser(args);
+            var startInfo = argParser.GetStartInfo();
+
+            if (startInfo.ShowHelp)
             {
-                return ShowConfig(QueryDisplayFlags.OnlyActivePaths);
+                PrintHelp();
+                return 0;
             }
-            var mode = args[0];
-            switch (mode)
+            if (startInfo.ShowVersion)
             {
-                case "record":
-                    if (args.Length != 2)
-                    {
-                        Console.Error.WriteLine("Expected a target filename (*.xml) as second command line argument.");
-                        return -1;
-                    }
-                    return Record(args[1]);
-                case "restore":
-                    if (args.Length != 2)
-                    {
-                        Console.Error.WriteLine("Expected a source filename (*.xml) as second command line argument.");
-                        return -1;
-                    }
-                    return Restore(args[1]);
-                default:
-                    Console.Error.WriteLine("Invalid mode. Expected 'record' or 'restore'.");
-                    return -1;
+                PrintVersionInfo();
+                return 0;
             }
+
+            var success = true;
+            if (startInfo.ShowConfig || startInfo.RecordTargetPath == null || startInfo.RestoreSourcePath == null)
+            {
+                success = success && ShowConfig(QueryDisplayFlags.OnlyActivePaths);
+            }
+            if (startInfo.RecordTargetPath != null)
+            {
+                success = success && Record(startInfo.RecordTargetPath);
+            }
+            if (startInfo.RestoreSourcePath != null)
+            {
+                success = success && Restore(startInfo.RestoreSourcePath, startInfo.PersistentRestore);
+            }
+            if (startInfo.Reset && startInfo.RestoreSourcePath == null)
+            {
+                success = success && Reset();
+            }
+            return success ? 0 : -1;
         }
 
-        static int SafeExecution(Action a)
+        static bool SafeExecution(Action a)
         {
             try
             {
                 a();
-                return 0;
+                return true;
             }
             catch (Exception ex)
             {
+#if DEBUG
                 Console.Error.WriteLine(ex);
-                return -1;
+#else
+                Console.Error.WriteLine(ex.Message);
+#endif
+                return false;
             }
         }
 
-        static int ShowConfig(QueryDisplayFlags flags) => SafeExecution(() =>
+        private static void PrintHelp()
+        {
+            PrintVersionInfo();
+            Console.WriteLine();
+            Console.WriteLine("Usage:");
+            Console.WriteLine("  DisplayMan");
+            Console.WriteLine("      No arguments: Show the current display configuration.");
+            Console.WriteLine("  DisplayMan <file path>");
+            Console.WriteLine("      One file path as the only argument:");
+            Console.WriteLine("      Load display configuration from XML file persistently.");
+            Console.WriteLine("  DisplayMan [<options>]*");
+            Console.WriteLine("      With options: See descriptions below.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine();
+            Console.WriteLine("  -h, --help, -?, /?");
+            Console.WriteLine("      Does print this help text. Ignores all other options.");
+            Console.WriteLine();
+            Console.WriteLine("  -v, --version");
+            Console.WriteLine("      Prints version information. Ignores all other options.");
+            Console.WriteLine();
+            Console.WriteLine("  -i, --info, --show-config");
+            Console.WriteLine("      Show the current display configuration.");
+            Console.WriteLine();
+            Console.WriteLine("  -s, --save, --record <target file>");
+            Console.WriteLine("      Write the current display configuration to an XML file.");
+            Console.WriteLine();
+            Console.WriteLine("  -l, --load, --restore <source file>");
+            Console.WriteLine("      Loads the display configuration from the specified XML file.");
+            Console.WriteLine("      If this option is used, the --reset option is ignored.");
+            Console.WriteLine("      By default, the configuration is loaded temporarily.");
+            Console.WriteLine();
+            Console.WriteLine("  -p, --persistent");
+            Console.WriteLine("      If loading a display configuration, makes the new configuration");
+            Console.WriteLine("      persistent.");
+            Console.WriteLine();
+            Console.WriteLine("  -r, --reset");
+            Console.WriteLine("      Resets the display configuration to the last persistent state.");
+            Console.WriteLine("      Can be used to switch back after loading a display configuration");
+            Console.WriteLine("      temporarily.");
+        }
+
+        private static void PrintVersionInfo()
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version.ToString(3);
+            Console.WriteLine("Mastersign DisplayManager");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Version: " + version);
+        }
+
+        static bool ShowConfig(QueryDisplayFlags flags) => SafeExecution(() =>
         {
             Console.WriteLine(Manager.QueryDisplayConfig(flags));
         });
 
-        static int Record(string fileName) => SafeExecution(() =>
+        static bool Record(string fileName) => SafeExecution(() =>
         {
             var config = Manager.QueryDisplayConfig(QueryDisplayFlags.OnlyActivePaths);
             var s = new XmlSerializer(typeof(DisplayConfiguration));
@@ -69,7 +131,7 @@ namespace Mastersign.DisplayManager
             }
         });
 
-        static int Restore(string fileName) => SafeExecution(() =>
+        static bool Restore(string fileName, bool persistent) => SafeExecution(() =>
         {
             var s = new XmlSerializer(typeof(DisplayConfiguration));
             DisplayConfiguration config;
@@ -84,7 +146,12 @@ namespace Mastersign.DisplayManager
             Manager.PatchDisplayConfig(config);
             Debug.WriteLine(config);
 
-            Manager.SetDisplayConfig(config);
+            Manager.SetDisplayConfig(config, persistent);
+        });
+
+        static bool Reset() => SafeExecution(() =>
+        {
+            Manager.ResetDisplayConfig();
         });
     }
 }
