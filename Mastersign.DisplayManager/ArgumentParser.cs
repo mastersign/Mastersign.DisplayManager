724f83b2f4013fb505b7ef4467051b9e017f0592
Tobias Kiertscher <dev@mastersign.de>
2017-12-15 13:04:10 +0100

    add new classes ArgumentParser and StartInfo

---
+++ Mastersign.DisplayManager/ArgumentParser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastersign.DisplayManager
+{
+    class ArgumentParser
+    {
+        private static readonly string[] HELP_FLAGS = new[] { "-h", "-?", "/?", "--help" };
+        private static readonly string[] VERSION_FLAGS = new[] { "-v", "--version" };
+        private static readonly string[] SHOW_CONFIG_FLAGS = new[] { "-i", "--info", "--show-config" };
+        private static readonly string[] RECORD_OPTIONS = new[] { "-s", "--save", "--record" };
+        private static readonly string[] RESTORE_OPTIONS = new[] { "-l", "--load", "--restore" };
+        private static readonly string[] PERSISTENT_RESTORE_FLAGS = new[] { "-p", "--persistent" };
+        private static readonly string[] RESET_FLAGS = new[] { "-r", "--reset" };
+
+        public string[] Arguments { get; private set; }
+
+        public ArgumentParser(string[] args)
+        {
+            Arguments = args;
+        }
+
+        public StartInfo GetStartInfo()
+        {
+            var showHelp = default(bool);
+            var showVersion = default(bool);
+            var showConfig = default(bool);
+            var recordTarget = default(string);
+            var restoreSource = default(string);
+            var persistentRestore = default(bool);
+            var reset = default(bool);
+
+            for (int i = 0; i < Arguments.Length; i++)
+            {
+                switch (Arguments[i])
+                {
+                    case var flag when HELP_FLAGS.Contains(flag):
+                        showHelp = true;
+                        break;
+                    case var flag when VERSION_FLAGS.Contains(flag):
+                        showVersion = true;
+                        break;
+                    case var flag when SHOW_CONFIG_FLAGS.Contains(flag):
+                        showConfig = true;
+                        break;
+                    case var flag when RECORD_OPTIONS.Contains(flag):
+                        i++;
+                        if (i < Arguments.Length)
+                        {
+                            recordTarget = Arguments[i];
+                        }
+                        break;
+                    case var flag when RESTORE_OPTIONS.Contains(flag):
+                        i++;
+                        if (i < Arguments.Length)
+                        {
+                            restoreSource = Arguments[i];
+                        }
+                        break;
+                    case var flag when PERSISTENT_RESTORE_FLAGS.Contains(flag):
+                        persistentRestore = true;
+                        break;
+                    case var flag when RESET_FLAGS.Contains(flag):
+                        reset = true;
+                        break;
+                }
+            }
+
+            return new StartInfo(
+                showHelp,
+                showVersion,
+                showConfig,
+                recordTarget,
+                restoreSource,
+                persistentRestore,
+                reset);
+        }
+    }
+}
---
+++ Mastersign.DisplayManager/StartInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastersign.DisplayManager
+{
+    class StartInfo
+    {
+        public bool ShowHelp { get; private set; }
+
+        public bool ShowVersion { get; private set; }
+
+        public bool ShowConfig { get; private set; }
+
+        public string RecordTargetPath { get; private set; }
+
+        public string RestoreSourcePath { get; private set; }
+
+        public bool PersistentRestore { get; private set; }
+
+        public bool Reset { get; private set; }
+
+        public StartInfo(
+            bool showHelp,
+            bool showVersion,
+            bool showConfig, 
+            string recordTargetPath, 
+            string restoreTargetPath, 
+            bool persistentRestore,
+            bool reset)
+        {
+            ShowHelp = showHelp;
+            ShowVersion = showVersion;
+            ShowConfig = showConfig;
+            RecordTargetPath = recordTargetPath;
+            RestoreSourcePath = restoreTargetPath;
+            PersistentRestore = persistentRestore;
+            Reset = reset;
+        }
+    }
+}
